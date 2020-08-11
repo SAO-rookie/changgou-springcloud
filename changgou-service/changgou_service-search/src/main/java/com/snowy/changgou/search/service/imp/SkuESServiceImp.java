ecad091a51fd4d10bf0b2f036843ee65d44afbb2
snowy <xueyuren109@gmail.com>
2020-08-11 22:52:28 +0800

    lambda写广告微服务项目

--- changgou-service/changgou_service-search/src/main/java/com/snowy/changgou/search/service/imp/SkuESServiceImp.java
+++ changgou-service/changgou_service-search/src/main/java/com/snowy/changgou/search/service/imp/SkuESServiceImp.java
@@ -1,5 +1,6 @@
 package com.snowy.changgou.search.service.imp;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.snowy.changgou.goods.entity.Sku;
 import com.snowy.changgou.goods.feign.SkuFeign;
@@ -7,6 +8,8 @@ import com.snowy.changgou.search.entity.SkuInfo;
 import com.snowy.changgou.search.mapper.SkuEsMapper;
 import com.snowy.changgou.search.service.SkuESService;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
@@ -14,10 +17,8 @@ import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author snowy
@@ -49,23 +50,83 @@ public class SkuESServiceImp implements SkuESService {
         String  keywords = Optional.ofNullable(searchMap.get("keywords")).orElse("华为");
         //2.创建查询对象 的构建对象
         NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
-
         //3.设置查询的条件
-
+        // 所有字段匹配
         nativeSearchQueryBuilder.withQuery(QueryBuilders.matchQuery("name", keywords));
-
+        //设置分组条件  商品分类
+        nativeSearchQueryBuilder.addAggregation(AggregationBuilders
+                .terms("skuCategorygroup")
+                .field("categoryName").size(50)
+                );
+        // 品牌 条件
+        nativeSearchQueryBuilder.addAggregation(AggregationBuilders
+                .terms("skuBrandgroup")
+                .field("brandName").size(50)
+        );
+        //设置分组条件  商品的规格
+        nativeSearchQueryBuilder.addAggregation(AggregationBuilders
+                .terms("skuSpecgroup")
+                .field("spec.keyword")
+                .size(100));
         //4.构建查询对象
         NativeSearchQuery query = nativeSearchQueryBuilder.build();
-
         //5.执行查询
         AggregatedPage<SkuInfo> skuPage = esTemplate.queryForPage(query, SkuInfo.class);
-
         //6.返回结果
+        //获取分组结果  商品分类
+        StringTerms stringTermsCategory = (StringTerms)skuPage.getAggregation("skuCategorygroup");
+        //获取分组结果  商品品牌
+        StringTerms stringTermsBrand = (StringTerms) skuPage.getAggregation("skuBrandgroup");
+        //获取分组结果  商品品牌
+        StringTerms stringTermsSpec = (StringTerms) skuPage.getAggregation("skuSpecgroup");
+
         Map resultMap = new HashMap<>();
         resultMap.put("rows", skuPage.getContent());
         resultMap.put("total", skuPage.getTotalElements());
         resultMap.put("totalPages", skuPage.getTotalPages());
-
+        resultMap.put("categoryList", dataProcessing(stringTermsCategory));
+        resultMap.put("brandList", dataProcessing(stringTermsBrand));
+        resultMap.put("specMap", getStringSetMap(stringTermsSpec));
         return resultMap;
     }
+
+    // 商品分类数据处理
+    private List<String>  dataProcessing(StringTerms stringTerms){
+      return  Optional.ofNullable(stringTerms)
+              .map(s -> s.getBuckets()
+                      .stream()
+                      .map(StringTerms.Bucket::getKeyAsString)
+                      .collect(Collectors.toList()))
+              .orElse(Collections.emptyList());
+    }
+
+    private Map<String, Set<String>> getStringSetMap(StringTerms stringTerms){
+        Map<String, Set<String>> specMap = new HashMap<String, Set<String>>();
+        Set<String> specList = Optional.ofNullable(stringTerms)
+                .map(s -> s.getBuckets()
+                        .stream()
+                        .map(StringTerms.Bucket::getKeyAsString)
+                        .collect(Collectors.toSet()))
+                .orElse(Collections.emptySet());
+
+        for (String specjson : specList) {
+            Map<String, String> map = JSON.parseObject(specjson, Map.class);
+            for (Map.Entry<String, String> entry : map.entrySet()) {//
+                String key = entry.getKey();        //规格名字
+                String value = entry.getValue();    //规格选项值
+                //获取当前规格名字对应的规格数据
+                Set<String> specValues = Optional.ofNullable(specMap.get(key))
+                        .orElse(new HashSet<String>())
+                        .stream()
+                        .collect(Collectors.toSet());
+                //将当前规格加入到集合中
+                specValues.add(value);
+                //将数据存入到specMap中
+                specMap.put(key, specValues);
+            }
+        }
+        return specMap;
+    }
+
+
 }
