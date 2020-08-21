007225fbe8ad4d6adfc0ad86b8351fe978be193d
snowy <xueyuren109@gmail.com>
2020-08-21 21:07:49 +0800

    令牌限流完成

---
+++ changgou-gateway/changgou-gateway-web/src/main/java/com/snowy/changgou/gateweyone/config/KeyResolverConfig.java
@@ -0,0 +1,32 @@
+package com.snowy.changgou.gateweyone.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author snowy
+ * @date 2020/8/20 20:25
+ */
+@Slf4j
+@Configuration
+public class KeyResolverConfig {
+    /***
+     * IP限流
+     * @return
+     */
+    @Bean(name="ipKeyResolver")
+    public KeyResolver userKeyResolver(){
+        return e->{
+            String hostAddress = e.getRequest().getRemoteAddress().getAddress().getHostAddress();
+            log.info(hostAddress);
+            return Mono.just(hostAddress);
+        };
+    }
+
+
+}
